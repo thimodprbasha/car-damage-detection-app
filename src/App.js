593ff7af409766a5c46977a399cd483a227e01d2
thimodprbasha <thimodprabasha@icloud.com>
2022-07-18 00:23:29 +0530

    fixed ui

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import { Header } from "./containers";
 import { Result } from "./components";
 import image from "./assets/image-bg.png";
 import Button from "@mui/material/Button";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
   const [file, setFile] = useState();
   const [submit, setSubmit] = useState(false);
+  const [isLoading, setLoading] = useState(false);
   function handleChange(e) {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
   function handleSubmit() {
-    setSubmit(true);
+    if (file != null) {
+      setLoading(true);
+      setTimeout(() => {
+        setSubmit(true);
+        setLoading(false);
+      }, 1000);
+    } else {
+    }
   }
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <div className="App">
       <Header />
@@ -22,38 +35,74 @@ function App() {
         <h1 style={{ color: "#FFFFFF", textAlign: "center", padding: "40px" }}>
           Submit Damage Assessment
         </h1>
-        <img src={file || image} width={500} height={400} />
-        <div className="button-upload">
-          <div className="button-upload-upload">
-            <Button
-              variant="contained"
-              component="label"
-              style={{ padding: "10px" }}
-            >
-              Upload
-              <input
-                hidden
-                accept="image/*"
-                type="file"
-                onChange={handleChange}
-              />
-            </Button>
+        <AlertToast></AlertToast>
+        {!isLoading ? (
+          <div>
+            <img src={file || image} width={500} height={400} />
+            <div className="button-upload">
+              {submit ? (
+                <div className="button-re-upload">
+                  <Button
+                    variant="contained"
+                    component="label"
+                    style={{ padding: "10px" }}
+                    onClick={refreshPage}
+                  >
+                    Reupload
+                  </Button>
+                </div>
+              ) : (
+                <>
+                  <div className="button-upload-upload">
+                    <Button
+                      variant="contained"
+                      component="label"
+                      style={{ padding: "10px" }}
+                    >
+                      Upload
+                      <input
+                        hidden
+                        accept="image/*"
+                        type="file"
+                        onChange={handleChange}
+                      />
+                    </Button>
+                  </div>
+                  <div className="button-upload-submit">
+                    <Button
+                      variant="contained"
+                      component="label"
+                      style={{ padding: "10px" }}
+                      onClick={handleSubmit}
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
-          <div className="button-upload-submit">
-            <Button
-              variant="contained"
-              component="label"
-              style={{ padding: "10px" }}
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
+        ) : (
+          <div className="spinner-container">
+            <div className="loading-spinner"></div>
           </div>
-        </div>
+        )}
+
         {submit && <Result />}
       </div>
     </div>
   );
 }
+
+const AlertToast = () => {
+  return (
+    <div>
+       <Alert variant="danger" dismissible>
+      <Alert.Heading>Please add image!</Alert.Heading>
+    </Alert>
+    </div>
+   
+  );
+};
 
 export default App;
